bdc78852fe9e4db8c5a09ef1d370eb526e0a8e59
rider <korn159@li.com>
2023-03-17 15:59:49 -0600

    DialogoDeInstalacion

---
+++ DialogoDeInstalacion.java
@@ -0,0 +1,14 @@
+// Importar librerías necesarias
+package dialogodeinstalacion;
+import javax.swing.JOptionPane;
+
+// Declarar la clase principal del programa
+public class DialogoDeInstalacion {
+
+    // Método principal del programa
+    public static void main(String[] args) {
+        
+        // Mostrar un mensaje de instrucción al usuario
+        JOptionPane.showMessageDialog(null, "Antes de comenzar la instalación, " + "cierre todas las aplicaciones.");
+    }
+}
